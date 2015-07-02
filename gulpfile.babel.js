12e08c1f56886558849a15e1ad8317e04f007e49
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2015-07-02 23:48:25 +0200

    CSS and JS minification

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,23 +1,44 @@
 'use strict';
 
-let config = {
-    src: 'src',
-    bowerComponents: 'src/bower_components'
+const config = {
+    src: 'app',
+    bowerComponents: 'bower_components'
 };
 
-let gulp = require('gulp'),
+const gulp = require('gulp'),
     connect = require('gulp-connect'),
-    bower = require('gulp-bower');
+    bower = require('gulp-bower'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    del = require('del'),
+    minifyCss = require('gulp-minify-css');
+
+gulp.task('clean', (cb) => del(['build'], cb));
 
 gulp.task('server', () => connect.server({
     livereload: true,
     root: config.src
 }));
 
-gulp.task('bower', () => bower().pipe(gulp.dest(config.bowerComponents)));
+gulp.task('bower', ['clean'], () => bower().pipe(gulp.dest(config.bowerComponents)));
+
+gulp.task('usemin', ['clean'], () => {
+    return gulp.src('app/*.html')
+        .pipe(usemin({
+            css: [minifyCss(), 'concat', rev()],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()]
+        }))
+        .pipe(gulp.dest('build/'));
+});
 
 gulp.task('watch', () =>
-    gulp.watch(config.src + '/**/*').on('change', (file) => gulp.src(file.path).pipe(connect.reload()))
+    gulp.watch(config.src + '/**/*').on('change', (file) => {
+        return gulp.src(file.path).pipe(connect.reload())
+    })
 );
+
+gulp.task('build', ['usemin']);
 
 gulp.task('default', ['bower', 'server', 'watch']);
